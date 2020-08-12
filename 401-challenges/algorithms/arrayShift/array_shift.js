5bb7a494adb56a1176538b897df235ac9f97b62d
Peng Chen <pchen0816@outlook.com>
2020-08-12 09:42:10 -0700

    class-02 modified

--- 401-challenges/algorithms/arrayShift/array_shift.js
+++ 401-challenges/algorithms/arrayShift/array_shift.js
@@ -4,16 +4,14 @@ function insertShiftArray(arr, val) {
   let mid = Math.ceil(arr.length/2);
   let temp = arr[mid];
   arr[mid]=val;
-  for (let i=mid; i<arr.length; i++){
-    if (i===mid){
-      if (temp===undefined){break;}
-      else{[arr[mid+1], temp] = [temp, arr[mid+1]];}
-    }
-    else {
-      if (temp===undefined){break;}
-      else {[arr[i+1],temp] = [temp, arr[i+1]];}
-    }
+  let len = arr.length;
+
+  for (let i=mid+1; i<=len; i++) {
+
+    [arr[i],temp] = [temp, arr[i]];
+
   }
+
   return arr;
 }
 
