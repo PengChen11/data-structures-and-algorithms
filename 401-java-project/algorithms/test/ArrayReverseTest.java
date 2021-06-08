a23adeb20486ebcb1593c1860141b5fa8aeb65dd
Chen <pngch@88665a231ac2.ant.amazon.com>
2021-06-08 15:07:02 -0700

    java challenge updated

--- 401-java-project/algorithms/test/ArrayReverseTest.java
+++ 401-java-project/algorithms/test/ArrayReverseTest.java
@@ -7,10 +7,6 @@ import static org.junit.Assert.*;
 
 public class ArrayReverseTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
     @Test
     public void reverseArray() {
         int[] test = {1,2,3,4,5,6,7,8};
@@ -50,6 +46,17 @@ public class ArrayReverseTest {
         System.out.println("\nreverseArray should reverse a given int array " + Arrays.toString(test));
         int[] actual = ArrayReverse.reverseArray(test);
         int[] expected = {};
+        assertArrayEquals("reverseArray should reverse a given int array" + Arrays.toString(test), expected,
+                actual);
+        System.out.println("reverseArray reversed the given int array to " + Arrays.toString(actual));
+    }
+
+    @Test
+    public void testReverseArray5(){
+        int[] test = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199};
+        System.out.println("\nreverseArray should reverse a given int array " + Arrays.toString(test));
+        int[] actual = ArrayReverse.reverseArray(test);
+        int[] expected = {199, 197, 193, 191, 181, 179, 173, 167, 163, 157, 151, 149, 139, 137, 131, 127, 113, 109, 107, 103, 101, 97, 89, 83, 79, 73, 71, 67, 61, 59, 53, 47, 43, 41, 37, 31, 29, 23, 19, 17, 13, 11, 7, 5, 3, 2};
         assertArrayEquals("reverseArray should reverse a given int array" + Arrays.toString(test), expected,
                 actual);
         System.out.println("reverseArray reversed the given int array to " + Arrays.toString(actual));
--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Part of growing as a programmer is to practice, practice, practice. Throughout C
 
 The 2nd 401 course with Code Fellows was JavaScript. The 1st time was done in Python, [click here to see the Python repo](https://github.com/PengChen11/python-data-structures-and-algorithms)
 
-Now it comes to the 3rd time, with **Java**. Each little challenges will be updated to include **Java** code in it. **NOTE:** The Java code is tested locally with Junit 4.13 and set up in IntelliJ.
+Now it comes to the 3rd time, with **Java**. Each little challenges will be updated to include **Java** code in it. 
+
+**NOTE:** The Java code is tested locally with Junit 4.13 and set up in IntelliJ.
 
 When doing these challenges this time, a few months after I started the Python 401 class,  I realized that my thought process is changed in the great way. This time some of my solutions are way much more efficient in O(time) and O(space).
 
